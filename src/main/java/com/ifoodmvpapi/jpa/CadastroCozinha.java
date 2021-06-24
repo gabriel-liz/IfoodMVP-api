19c64ce33e6e53f513430e2a4f41a2d9fa6131d1
GabrielAssumpcaoLiz <gabriel.liz@univille.br>
2021-06-24 11:40:50 -0300

    incluido metodo buscar de exemplo

--- src/main/java/com/ifoodmvpapi/jpa/CadastroCozinha.java
+++ src/main/java/com/ifoodmvpapi/jpa/CadastroCozinha.java
@@ -24,6 +24,11 @@ public class CadastroCozinha {
 				.getResultList();			
 
 	}
+	
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+	
 	@Transactional
 	public Cozinha adicionar(Cozinha cozinha) {
 		return manager.merge(cozinha);
